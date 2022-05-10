e8e0f8126bfa51787a05bbf59b7ff81b0e612330
Shahriar <meherss@uio.no>
2022-05-10 17:42:21 +0200

    laster opp for gutta

--- GUI.java
+++ GUI.java
@@ -5,10 +5,10 @@ import java.util.ArrayList;
 
 public class GUI implements ActionListener {
     JFrame hovedVindu;
-    JPanel hovedPanel, oppePanel, verdenPanel, tittelPanel, tomPanel; // TODO
-    // JLabel m;
+    JPanel hovedPanel, oppePanel, verdenPanel, tittelPanel, tomPanel, spill; // TODO
     char rettning; // TODO finish
     JButton opp, ned, hoyre, venstre, slutt;
+    JLabel[][] bokser = new JLabel[12][12]; 
 
     GUI() {
         try {
@@ -33,33 +33,87 @@ public class GUI implements ActionListener {
         tittelPanel.setSize(500, 30);
         tittelPanel.setBounds(100, 5, 500, 30);
         // tittelPanel.setAlignmentX(JPanel.CENTER_ALIGNMENT);
-        tittelPanel.setBorder(BorderFactory.createLineBorder(Color.BLACK, 3));
+        //tittelPanel.setBorder(BorderFactory.createLineBorder(Color.BLACK, 3));
         JLabel testTest = new JLabel("Snake");
         testTest.setHorizontalAlignment(JLabel.CENTER);
         testTest.setVerticalAlignment(JLabel.CENTER);
         testTest.setForeground(Color.green);
 
         oppePanel = new JPanel();
+        oppePanel.setBounds(50, 40, 600, 200);
         oppePanel.setSize(600, 200);
-        oppePanel.setBounds(50, 60, 600, 200);
+        oppePanel.setLayout(new GridLayout(0,2));
         oppePanel.setBorder(BorderFactory.createLineBorder(Color.BLACK, 3));
         oppePanel.setBackground(Color.gray);
 
-        // oppePanel.setForeground(new Color(255, 0, 0));
-        // oppePanel.setBackground(new Color(255, 0, 0));
+        JLabel lengde = new JLabel("Lengde: 1");
+        lengde.setHorizontalAlignment(JLabel.CENTER);
+        oppePanel.add(lengde);
 
+        JPanel knapper = new JPanel();
+        knapper.setLayout(new BorderLayout());
+        opp = new JButton("opp");
+        ned = new JButton("ned");
+        hoyre = new JButton("hoyre");
+        venstre = new JButton("venstre");
+        slutt = new JButton("exit");
+        knapper.add(opp, BorderLayout.NORTH);
+        knapper.add(ned, BorderLayout.SOUTH);
+        knapper.add(hoyre, BorderLayout.EAST);
+        knapper.add(venstre, BorderLayout.WEST);
+        knapper.add(slutt, BorderLayout.CENTER);
+
+        opp.addActionListener(this);
+        ned.addActionListener(this);
+        hoyre.addActionListener(this);
+        venstre.addActionListener(this);
+        slutt.addActionListener(this);
+
+
+        oppePanel.add(knapper);
+        
+        spill = new JPanel();
+        spill.setSize(700, 495);
+        spill.setBounds(65, 240, 570, 570);
+        spill.setLayout(new GridLayout(12,12));
+        spill.setBackground(Color.gray);
+        spill.setBorder(BorderFactory.createLineBorder(Color.BLACK, 3));
+        for (int y = 1; y <=12; y++) {
+            for (int x = 1; x <= 12; x++) {
+                JLabel box = new JLabel(" ");
+                box.setBorder(BorderFactory.createLineBorder(Color.BLACK));
+                bokser[y-1][x-1] = box;
+                spill.add(box);
+            }
+        }
         hovedVindu.add(hovedPanel);
-        // hovedPanel.add(tomPanel);
         tittelPanel.add(testTest);
         hovedPanel.add(oppePanel);
+        hovedPanel.add(spill);
         hovedPanel.add(tittelPanel);
         hovedVindu.setVisible(true);
-
     }
-
     @Override
     public void actionPerformed(ActionEvent e) {
-        // TODO Auto-generated method stub
+        if(e.getSource() == slutt){
+            System.exit(0);
+        }
+        else if(e.getSource() == opp){
+            System.out.println("du trakk opp");
+        }
+        else if(e.getSource() == ned){
+            System.out.println("du trakk ned");
+        }
+        else if(e.getSource() == hoyre){
+            System.out.println("du trakk hoyre");
+        }
+        else if(e.getSource() == venstre){
+            System.out.println("du trakk venstre");
+        }
+    }
 
+    public void startpos(){
+        JLabel start = bokser[6][6];
+        //start.setText(text);
     }
 }
